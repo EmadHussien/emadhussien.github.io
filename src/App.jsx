7ac9a83062851b5876a1dddf95345e232cb96bd2
Emad Hussien <emadhussien851@gmail.com>
2024-07-26 03:29:24 +0300

    uploading the new site

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import TerminalController from './TerminalController.jsx';
+
+function App() {
+  return (
+    <>
+      <TerminalController name="Emad" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/TerminalController.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import Terminal, {
+  ColorMode,
+  TerminalOutput,
+  TerminalInput,
+} from 'react-terminal-ui';
+
+const TerminalController = (props = {}) => {
+  const [terminalLineData, setTerminalLineData] = useState([
+    <TerminalOutput>
+      Welcome to Emad's website. As you can see, you can use the terminal.
+      <br />
+      Have fun, and don't hesitate to use 'help' if you need it!
+    </TerminalOutput>,
+  ]);
+
+  const handleTerminalInput = (terminalInput) => {
+    let newLineData = [...terminalLineData];
+    newLineData.push(<TerminalInput>{terminalInput}</TerminalInput>);
+    switch (terminalInput.trim().toLowerCase()) {
+      case 'help':
+        newLineData.push(
+          <TerminalOutput>
+            Available commands are email,cv, github, linkedin, clear' will clear
+            the terminal.
+          </TerminalOutput>
+        );
+        break;
+      case 'cv':
+        newLineData.push(
+          <TerminalOutput>
+            <a
+              style={{ color: 'white', fontSize: '15px' }}
+              href="https://emadhussien.github.io/mycv/Emad_Hussien_Software_Engineer_Resume.pdf"
+              target="_blank"
+            >
+              https://emadCv.com
+            </a>
+          </TerminalOutput>
+        );
+        break;
+      case 'github':
+        newLineData.push(
+          <TerminalOutput>
+            <a
+              style={{ color: 'white', fontSize: '15px' }}
+              href="https://github.com/EmadHussien"
+              target="_blank"
+            >
+              https://github.com/EmadHussien
+            </a>
+          </TerminalOutput>
+        );
+        break;
+
+      case 'linkedin':
+        newLineData.push(
+          <TerminalOutput>
+            <a
+              style={{ color: 'white', fontSize: '15px' }}
+              href="https://www.linkedin.com/in/emadhussien98"
+              target="_blank"
+            >
+              https://www.linkedin.com/in/emadhussien98
+            </a>
+          </TerminalOutput>
+        );
+        break;
+      case 'email':
+        newLineData.push(
+          <TerminalOutput>
+            <a
+              style={{ color: 'white', fontSize: '15px' }}
+              href="mailto:emadhussien.fcis@gmail.com"
+            >
+              emadhussien.fcis@gmail.com
+            </a>
+          </TerminalOutput>
+        );
+        break;
+      case 'clear':
+        newLineData = [];
+        break;
+      default:
+        newLineData.push(
+          <TerminalOutput>
+            Unknown command. Type 'help' for available commands.
+          </TerminalOutput>
+        );
+    }
+    setTerminalLineData(newLineData);
+  };
+
+  return (
+    <div className="container" style={{ height: '100%' }}>
+      <Terminal
+        height="640px"
+        colorMode={ColorMode.Dark}
+        onInput={handleTerminalInput}
+        prompt=">"
+      >
+        {terminalLineData}
+      </Terminal>
+    </div>
+  );
+};
+
+export default TerminalController;
